7c6ace11c1e796aef7a8292171c8a1297b9f02dc
Julian Trebach <juliantreb@gmail.com>
2019-06-25 10:18:01 +0100

    push

--- index.js
+++ index.js
@@ -25,6 +25,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.get("/ping", (req,res) => {
+    res.send("Pong");
+});
 
 app.use(UserRoutes);
 app.use(ProviderRoutes);
